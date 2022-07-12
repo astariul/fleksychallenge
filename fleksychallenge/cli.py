b5a0bffb0babb464200cd732b685df1d02b41f96
Astariul <remondnicola@gmail.com>
2022-07-12 22:18:58 +0900

    :sparkles: Add test script

--- fleksychallenge/cli.py
+++ fleksychallenge/cli.py
@@ -1,21 +1,24 @@
 import argparse
 
-from fleksychallenge import interact, prepare, train
+from fleksychallenge import interact, prepare, test, train
 
 
 def cli():
     parser = argparse.ArgumentParser(description="Fleksy challenge part 1")
-    parser.add_argument("cmd", help="Command to execute", choices=["prepare", "train", "interact"])
+    parser.add_argument("cmd", help="Command to execute", choices=["prepare", "train", "test", "interact"])
     parser.add_argument("--dataset", help="Where to save/load the dataset from", default="tweet_dataset")
     parser.add_argument("--config", help="Location of the config file to use for training", default="config.cfg")
     parser.add_argument("--model", help="Where to save/load the model from", default="sentiment_model")
     parser.add_argument("--gpu", action="store_true")
+    parser.add_argument("--full", action="store_true")
     args = parser.parse_args()
 
     if args.cmd == "prepare":
         prepare(args)
     elif args.cmd == "train":
         train(args)
+    elif args.cmd == "test":
+        test(args)
     elif args.cmd == "interact":
         interact(args)
 
