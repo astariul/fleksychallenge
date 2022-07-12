0bfb03755de20a44949dc9552fe83b34d9b97619
Astariul <remondnicola@gmail.com>
2022-07-12 21:46:56 +0900

    :sparkles: Add training function

--- fleksychallenge/cli.py
+++ fleksychallenge/cli.py
@@ -7,6 +7,9 @@ def cli():
     parser = argparse.ArgumentParser(description="Fleksy challenge part 1")
     parser.add_argument("cmd", help="Command to execute", choices=["prepare", "train", "interact"])
     parser.add_argument("--dataset", help="Where to save/load the dataset from", default="tweet_dataset")
+    parser.add_argument("--config", help="Location of the config file to use for training", default="config.cfg")
+    parser.add_argument("--model", help="Where to save/load the model from", default="sentiment_model")
+    parser.add_argument("--gpu", action="store_true")
     args = parser.parse_args()
 
     if args.cmd == "prepare":
